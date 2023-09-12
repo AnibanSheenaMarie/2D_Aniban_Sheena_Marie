e87cb8e995bd6e36fa4453454030d01acea4e22c
Sheena Marie Aniban <116483466+AnibanSheenaMarie@users.noreply.github.com>
2023-09-12 22:21:28 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package BSIT_2D_Activity1;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Sheena Marie", 20, 70000);
+        Employee emp2 = new Employee("Sheena", 20, 80000);
+        Employee emp3 = new Employee("Rey-ann", 32, 120000);
+
+        System.out.println(emp1);
+        System.out.println(emp2);
+        System.out.println(emp3);
+    }
+}
