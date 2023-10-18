9df373b4ae333b27f34d3f6217ed7e8d78d6f4cf
Sheena Marie Aniban <116483466+AnibanSheenaMarie@users.noreply.github.com>
2023-10-18 23:29:47 +0800

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,23 @@
+class book{
+private string title;
+private int numberofPages;
+private boolean is fictional;
+
+public String getTitle(){
+return title;
+}
+public String getnum_pages(){
+return num_pages;
+}
+public boolean getIs_fiction(){
+return is_fiction;
+}
+
+public void setTitle(){
+this.title;
+}
+public void setgetnumofpages(){
+this.getnumofpages;
+}
+public boolean setIs_fiction(){
+this.boolean is fiction;
