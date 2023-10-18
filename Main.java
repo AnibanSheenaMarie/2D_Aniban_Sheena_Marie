1269ea82d71773eecb102a6d14a4198b9caab0f5
Sheena Marie Aniban <116483466+AnibanSheenaMarie@users.noreply.github.com>
2023-10-18 23:31:20 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import.java.util.scanner;
+
+class main(){
+public static void main(String[]args){
+
+Book book= new book();
+scanner scan=new scanner(system.in);
+
+system.out.println("Enter book title:");
+book.title=scan.nextline();
+system.out.println("Enter book number of pages:");
+book.num_pages=scan.nextline();
+
+ boolean is fictional=false;
+
+
+if(is fictional){
+ system.out.println("its fiction");
+}else{
+system.out.println("its not fiction:");
+  }
